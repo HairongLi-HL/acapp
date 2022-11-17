851bff07332462bf8166b2cd5c70329f194f119a
hairong li <hairongli88@gmail.com>
2022-11-17 14:05:00 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style = "text-align: center"> A Web Here</h1>'
+    line2 = '<img src = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg">'
+    return HttpResponse(line1 + line2)
